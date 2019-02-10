3ff09987a48727173f5f4cb4bb612f9b08afef9b
Marek Tomko <mtomko@sk.ibm.com>
2019-02-10 11:22:47 +0100

    refactor(postProcessMp4Audio): Chain results and get sorted list of transcripts

--- routes/v1/mobile/modules/postProcessMp4Audio.js
+++ routes/v1/mobile/modules/postProcessMp4Audio.js
@@ -1,6 +1,8 @@
 const Immutable = require('immutable');
 const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
 const configParams = Immutable.fromJS(require('../../../../config.dev.json'));
+const _ = require('lodash');
+const logger = require('pino')()
 
 module.exports = (req, res, next) => {
     const speechToText = new SpeechToTextV1({
@@ -17,10 +19,34 @@ module.exports = (req, res, next) => {
 
     speechToText.recognize(recognizeParams, (error, results) => {
         if(error) {
-            res.send(error);
+            logger.error(error);
+            res.status(400).send({
+                error: true,
+                transcripts: [],
+                error: error.message
+            });
         } else {
-            console.log(results);
-            res.status(200).send(results);
+            let transcripts = _
+                .chain(_.get(results, 'results'))
+                .reduce((acc, item) => {
+                    acc.push(_.get(item, 'alternatives').map(alternative => {
+                        return {
+                            transcript: _.get(alternative, 'transcript'),
+                            confidence: _.get(alternative, 'confidence')
+                        }
+                    }));
+                    return acc;
+                }, [])
+                .flattenDeep()
+                .sortBy('confidence')
+                .reverse()
+                .value()
+            logger.info(results);
+            res.status(200).send({
+                error: false,
+                transcripts: transcripts,
+                error: 'All good'
+            });
         }
     })
 }
