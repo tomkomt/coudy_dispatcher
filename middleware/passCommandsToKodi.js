493af24df6246193a8c3be9d56e13ff7e3726383
Marek Tomko <MTomko@sk.ibm.com>
2019-03-07 17:29:34 +0100

    Add middleware support to command processor

--- middleware/passCommandsToKodi.js
+++ middleware/passCommandsToKodi.js
@@ -24,25 +24,42 @@ module.exports = (req, res, next) => {
     logger.info('Commands: ', commands)
 
     eachSeries(commands, (command_item, callback) => {
-        let request_body = {
-            "jsonrpc": "2.0",
-            "method": configParams.getIn(['services', 'kodi_jsonrpc', 'methods', command_item, 'method']),
-            "params": configParams.getIn(['services', 'kodi_jsonrpc', 'methods', command_item, 'params']).toJS(),
-            "id": 1
-        }
+        let command_config = configParams.getIn(['services', 'kodi_jsonrpc', 'methods', command_item])
+        switch(command_config.get('type')) {
+            case 'method':
+                let request_body = {
+                    "jsonrpc": "2.0",
+                    "method": command_config.get('method'),
+                    "params": command_config.get('params').toJS(),
+                    "id": 1
+                }
+        
+                unirest.post(configParams.getIn(['services', 'kodi_jsonrpc', 'url']))
+                .headers({
+                    'Content-Type': 'application/json'
+                })
+                .send(request_body)
+                .end((response) => {
+                    if(_.get(response, 'status') == 200) {
+                        callback(null, _.get(response, 'body'));
+                    } else {
+                        callback(response.error)
+                    }
+                })
+            break;
 
-        unirest.post(configParams.getIn(['services', 'kodi_jsonrpc', 'url']))
-        .headers({
-            'Content-Type': 'application/json'
-        })
-        .send(request_body)
-        .end((response) => {
-            if(_.get(response, 'status') == 200) {
-                callback(null, _.get(response, 'body'));
-            } else {
-                callback(response.error)
-            }
-        })
+            case 'middleware':
+                require(`../routine/${command_config.get('method')}.js`)().then(results => {
+                    callback(null, results);
+                }).catch(error => {
+                    callback(error.message);
+                })
+            break;
+
+            default:
+                callback(null, [])
+            break;
+        }
     }, (error, cb_results) => {
         if(error) {
             res.status(402).send({
